873ef83bb8aeba919e19431de5c58663cd47f5a7
youichi yamazaki <denncoru@yahoo.co.jp>
2019-10-24 17:59:51 +0900

    タスク一覧画面(index.html.erb)を作成、コントローラにindexアクションの内容を記述

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,23 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを作成しました"
+    else
+      render :new
   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_name, :content)
+  end
+end
