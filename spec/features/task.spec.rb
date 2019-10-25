555c17cd45f200b2b191d389e0ebbb239da70a8d
youichi yamazaki <denncoru@yahoo.co.jp>
2019-10-25 16:37:35 +0900

    ❶ specフォルダ内にfeaturesフォルダ、その中にfeature spec用のファイルtask.spec.rbを作成 ❷ task.spec.rbに現状のタスク機能に対してE2Eテストを記述

---
+++ spec/features/task.spec.rb
@@ -0,0 +1,29 @@
+# このrequireで、Capybaraなどの、Feature Specに必要な機能を使用可能な状態にしています
+require 'rails_helper'
+
+# このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
+RSpec.feature "タスク管理機能", type: :feature do
+  scenario "タスク一覧のテスト" do
+    Task.create!(task_name: 'test_task_01', content: 'test')
+    Task.create!(task_name: 'test_task_02', content: 'sample')
+    visit tasks_path
+    expect(page).to have_content 'test'
+    expect(page).to have_content 'sample'
+  end
+
+  scenario "タスク作成のテスト" do
+    visit new_task_path
+    fill_in 'Task name', with: 'test_task'
+    fill_in 'Content', with: 'test'
+    click_on 'タスクを更新'
+    expect(page).to have_content 'test_task'
+    expect(page).to have_content 'test'
+  end
+
+  scenario "タスク詳細のテスト" do
+    @task = Task.create!(task_name: 'test_task', content: 'test')
+    visit task_path(@task)
+    expect(page).to have_content 'test_task'
+    expect(page).to have_content 'test'
+  end
+end
