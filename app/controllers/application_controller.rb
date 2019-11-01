62c8e02f8b114a48923af036abde72cfa778ddd1
youichi yamazaki <denncoru@yahoo.co.jp>
2019-11-01 11:07:04 +0900

    Bassic認証を実装するためapplication_controllerにコードを記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_authentication
+
+  private
+
+  def basic_authentication
+    authenticate_or_request_with_http_basic do |name, password|
+      name == "admin" && password == "admin_password"
+    end
+  end
 end
