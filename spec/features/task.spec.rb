af78589ab9e22d63d86931b83b640594a4752316
youichi yamazaki <denncoru@yahoo.co.jp>
2019-11-06 11:37:28 +0900

    task.spec.rbの各テストに優先順位が追加された際にもテストが通るよう追記

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -4,9 +4,9 @@ RSpec.feature "タスク管理機能", type: :feature do
 
   background do
     FactoryBot.create(:second_task, task_name: 'test1', content: 'test1', created_at: Time.current + 1.days, deadline: '2019-11-14', status: '未着手', priority: '高')
-    FactoryBot.create(:second_task, task_name: 'test2', content: 'test2', created_at: Time.current + 2.days, deadline: '2019-11-15', status: '未着手', priority: '高')
-    FactoryBot.create(:second_task, task_name: 'test3', content: 'test3', created_at: Time.current + 3.days, deadline: '2019-11-16', status: '未着手', priority: '高')
-    FactoryBot.create(:second_task, task_name: 'test4', content: 'test4', created_at: Time.current + 4.days, deadline: '2019-11-17', status: '未着手', priority: '高')
+    FactoryBot.create(:second_task, task_name: 'test2', content: 'test2', created_at: Time.current + 2.days, deadline: '2019-11-15', status: '未着手', priority: '低')
+    FactoryBot.create(:second_task, task_name: 'test3', content: 'test3', created_at: Time.current + 3.days, deadline: '2019-11-16', status: '未着手', priority: '中')
+    FactoryBot.create(:second_task, task_name: 'test4', content: 'test4', created_at: Time.current + 4.days, deadline: '2019-11-17', status: '未着手', priority: '中')
   end
   scenario "タスク一覧のテスト" do
     visit tasks_path
@@ -19,7 +19,7 @@ RSpec.feature "タスク管理機能", type: :feature do
     fill_in 'タスク名', with: 'test_task'
     fill_in '内容', with: 'test'
     fill_in 'Deadline', with: '2019-11-14'
-    fill_in 'Priority', with: '高'
+    select '高', from: 'Priority'
     click_on 'タスクを作成'
     expect(page).to have_content 'test_task'
     expect(page).to have_content 'test'
@@ -29,11 +29,13 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク詳細のテスト" do
-    @task = Task.create!(task_name: 'test_task', content: 'test', deadline: '2019-11-14')
+    @task = Task.create!(task_name: 'test_task', content: 'test', deadline: '2019-11-14', status: '未着手', priority: '高')
     visit task_path(@task)
     expect(page).to have_content 'test_task'
     expect(page).to have_content 'test'
     expect(page).to have_content '2019-11-14'
+    expect(page).to have_content '未着手'
+    expect(page).to have_content '高'
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
@@ -48,7 +50,14 @@ RSpec.feature "タスク管理機能", type: :feature do
     click_on '終了期限でソートする'
     task = all('tr td')
     task_0 = task[3]
-    save_and_open_page
     expect(task_0).to have_content '2019-11-17'
+  end
+
+  scenario '「優先順位でソートする」を押すと終了期限の降順でソートされるかのテスト' do
+    visit tasks_path
+    click_on '優先順位でソートする'
+    task = all('tr td')
+    task_0 = task[5]
+    expect(task_0).to have_content '高'
   end
 end
