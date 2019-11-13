8fd1e4a8d6330d81e6cb2bb8d6b72d4acd229ba1
youichi yamazaki <denncoru@yahoo.co.jp>
2019-11-13 10:59:14 +0900

    seedを用いてユーザーを1人作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root 'tasks#index'
 
   resources :users
-  resources :sessions only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 end
