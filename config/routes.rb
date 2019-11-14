b443a46638a505c964ba6bd937d1b4bf32261a3c
youichi yamazaki <denncoru@yahoo.co.jp>
2019-11-14 14:08:22 +0900

    ❶ ユーザーが管理者かどうか見分けるため、Userモデルにadminフラグを追加 ❷ ユーザー管理のためにadminコントローラを作成　❸ admin用のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/show'
+    get 'users/index'
+  end
   get 'sessions/new'
   resources :tasks
   root 'tasks#index'
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+
+  namespace :admin do
+    resources :users
+  end
 end
